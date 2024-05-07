c381194a2f7e8821ff62fa51b4c97908e5914105
StephanieMfon <okpomfonstephanie@gmail.com>
2024-05-07 18:50:00 +0100

    added new changes

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,5 +8,5 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """A generator that yields random numbers bet 0 and 10"""
     for _ in range(10):
-        asyncio.sleep(1)
+        await asyncio.sleep(1)
         yield random.random() * 10
